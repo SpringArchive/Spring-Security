e7111f3788846ae4cf07b5ee65a8666dcf7a520d
KoungQ <guu119@naver.com>
2024-01-26 15:51:29 +0900

    Section2

--- src/main/java/com/eazybytes/SpringsecuritybasicApplication.java
+++ src/main/java/com/eazybytes/SpringsecuritybasicApplication.java
@@ -1,11 +1,11 @@
-package com.eazybytes.springsecuritybasic;
+package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.eazybytes.springsecuritybasic.controller")	// Optional
+@ComponentScan("com.eazybytes.controller")	// Optional
 public class SpringsecuritybasicApplication {
 
 	public static void main(String[] args) {
