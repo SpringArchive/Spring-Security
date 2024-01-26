6334fd16fac7ad2f79d5c8eaeef3d36f9b13c3df
KoungQ <guu119@naver.com>
2024-01-26 17:41:20 +0900

    Section 2

--- src/main/java/com/eazybytes/controller/NoticesController.java
+++ src/main/java/com/eazybytes/controller/NoticesController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+public class NoticesController {
 
-    @GetMapping("/welcome")
-    public String sayWelcome() {
-        return "Welcome to Spring Application with Security";
+    @GetMapping("/notices")
+    public String getNotices() {
+        return "Here are the notices details from the DB";
     }
 
 }
