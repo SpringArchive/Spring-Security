a77a0c85f831d9098793db53baf545c207ab8f19
KoungQ <guu119@naver.com>
2024-04-21 19:19:35 +0900

    Section 3

--- Section3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ Section3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -18,57 +16,62 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
-                .requestMatchers("/notices","/contact").permitAll())
+        http.csrf(AbstractHttpConfigurer::disable)  // csrf 해제 (람다식 or 메서드 참조 표현식으로 deprecated 메서드 살림)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                        .requestMatchers("/notices","/contact","/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
-
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-
-        /**
-         *  withDefaultPasswordEncoder() 는 Spring 팀에서 추후에 삭제할 생각은 없지만
-         *  Product 개발할 때 사용하지 않는 것을 권함
-         *
-         *  UserDetails = 인터페이스
-         *  User = 클래스
-         *  이 둘은 서로 부모자식 관계가 되기 때문에 type cast 가능
-         *
-         *  NoOpPasswordEncoder() 는 비밀번호가 일반 텍스트로 처리돼서 일반적으로 사용하지 않는 것을 권함
-         */
-
-//        // Approach 1
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//
+//        /**
+//         *  withDefaultPasswordEncoder() 는 Spring 팀에서 추후에 삭제할 생각은 없지만
+//         *  Product 개발할 때 사용하지 않는 것을 권함
+//         *
+//         *  UserDetails = 인터페이스
+//         *  User = 클래스
+//         *  이 둘은 서로 부모자식 관계가 되기 때문에 type cast 가능
+//         *
+//         *  NoOpPasswordEncoder() 는 비밀번호가 일반 텍스트로 처리돼서 일반적으로 사용하지 않는 것을 권함
+//         */
+//
+////        // Approach 1
+////        UserDetails admin = User.withDefaultPasswordEncoder()
+////                .username("admin")
+////                .password("12345")
+////                .authorities("admin")
+////                .build();
+////
+////        UserDetails user = User.withDefaultPasswordEncoder()
+////                .username("user")
+////                .password("12345")
+////                .authorities("read")
+////                .build();
+////
+////        return new InMemoryUserDetailsManager(admin, user);
+//
+//        // Approach 2
+//        UserDetails admin = User.withUsername("admin")
 //                .password("12345")
 //                .authorities("admin")
 //                .build();
 //
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
+//        UserDetails user = User.withUsername("user")
 //                .password("12345")
 //                .authorities("read")
 //                .build();
 //
 //        return new InMemoryUserDetailsManager(admin, user);
+//    }
 
-        // Approach 2
-        UserDetails admin = User.withUsername("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-
-        UserDetails user = User.withUsername("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
